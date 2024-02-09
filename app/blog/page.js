5c7c4c1de89b3ea1fad2164e84b887ef31cb7608
nikhilnigamnik <nikhilnigamnik@gmail.com>
2024-02-09 23:52:22 +0530

    fix image in blog

--- app/blog/page.js
+++ app/blog/page.js
@@ -25,7 +25,7 @@ const page = () => {
 
   return (
     <Section>
-     <ClarityContainer projectId={"kz5pyv8z7k"}></ClarityContainer>
+      <ClarityContainer projectId={"kz5pyv8z7k"}></ClarityContainer>
       <div className="flex flex-col gap-3 justify-center items-center">
         <Tag>Blog</Tag>
         <h2 className="text-3xl font-bold">Latest Blog</h2>
@@ -33,7 +33,13 @@ const page = () => {
       <div className="grid gap-6 gap-y-10 py-6 md:grid-cols-2 lg:grid-cols-3">
         {posts.map((post) => (
           <div key={post.title} className="borders bg p-4 rounded-xl">
-            <Image width={200}  src={post?.banner} className="rounded-xl" alt="blogPoster" />
+            <Image
+              width={500}
+              height={500}
+              src={post?.banner}
+              className="rounded-xl"
+              alt="blogPoster"
+            />
             <div className="min-h-min p-3">
               <p className="mt-4 w-full text-xs font-semibold leading-tight text-gray-400">
                 #{post.category}
